41f7ff307b32d60fa816ec2ca5052721f10c12da
benndora1 <benndora2@gmail.com>
2022-04-21 12:33:21 +0300

    more pract

---
+++ prac.py
@@ -0,0 +1,49 @@
+# for i in range(10):
+#     if i % 2:
+#         continue
+#     print(i)
+
+# Using Python continue in a while loop example
+
+# counter = 0
+# while counter < 10:
+#     counter += 1
+#     if not counter % 2:
+#         continue
+#     print(counter)
+
+# Introduction to the Python pass statement
+# counter = 1
+
+# max = 10
+
+# if counter <= max:
+#     counter += 1
+
+# else:
+#     # implement later
+
+#     pass # The pass statement is a statement that does nothing. It’s just a placeholder for the code that you’ll write in the future.
+
+# What is a function
+
+# A function is a named code block that performs a job or returns a value.
+
+# def greet():
+#     """ display a greeting message """
+#     print("Hello Benson")
+# def add(a, b):
+#     """ Add two arguments
+#     Arguments:
+#         a: an integer
+#         b: an integer
+#     Returns:
+#         The sum of the two arguments
+#     """
+#     return a + b
+
+def greet(name):
+
+    pass
+
+print(greet(name))
